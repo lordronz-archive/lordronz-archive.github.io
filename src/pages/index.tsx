9317bbe602b5514de11681bcedf53180d79d769e
LordRonz <aarontanhar2000@gmail.com>
2021-12-26 15:33:03 +0700

    feat: particle component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@
 import type { NextPage } from 'next';
 
 import Footer from '@/components/Footer';
-import ButtonLink from '@/components/links/ButtonLink';
+import ArrowLink from '@/components/links/ArrowLink';
 import CustomLink from '@/components/links/CustomLink';
+import ParticleComponent from '@/components/ParticleComponent';
 import Seo from '@/components/Seo';
 
 const Home: NextPage = () => {
@@ -11,23 +12,33 @@ const Home: NextPage = () => {
     <>
       <Seo />
       <main>
-        <section className='bg-black text-primary-50'>
+        <ParticleComponent />
+        <section className='bg-transparent text-primary-50'>
           <div className='layout flex flex-col justify-center items-center min-h-screen text-center'>
-            <h1>LordRonz{"'"}s Archive repository</h1>
-            <p className='mt-2 text-md text-primary-50'>
+            <h1 className='z-10'>LordRonz{"'"}s Archive</h1>
+            <p className='mt-2 text-md text-primary-50 z-10'>
               The purpose of this account is to store{' '}
               <CustomLink href='https://github.com/LordRonz/'>my main account{"'"}s</CustomLink> archived repo so that it
               doesn{"'"}t clutter the repository list.
             </p>
-            <p className='mt-2 text-md text-primary-50'>
+            <p className='text-md text-primary-50 z-10'>
+              AND to fulfill someone{"'"}s desire for the{' '}
+              <CustomLink href='https://github.com/matteobruni/tsparticles'>particle component</CustomLink>.
+            </p>
+            <p className='mt-4 text-md text-primary-50 z-10'>
               <CustomLink href='https://github.com/orgs/lordronz-archive/repositories'>See the repositories</CustomLink>
             </p>
 
-            <p className='mt-2 text-md text-primary-50'>Below are my github accounts</p>
+            <p className='mt-4 text-md text-primary-50 z-10'>Below are my github accounts</p>
 
-            <ButtonLink className='mt-6' href='#' variant='primary'>
-              Work In Progress
-            </ButtonLink>
+            <div className='z-10 flex gap-x-10 mt-4'>
+              <ArrowLink className='mt-6' href='https://github.com/lordronz/' variant='primary'>
+                LordRonz
+              </ArrowLink>
+              <ArrowLink className='mt-6' href='https://github.com/znordrol/' variant='primary'>
+                znordrol
+              </ArrowLink>
+            </div>
 
             <Footer />
           </div>
