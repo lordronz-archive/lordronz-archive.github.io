ebc00271f7edb70309d2aa91d6d6b267c9fb93b2
LordRonz <aarontanhar2000@gmail.com>
2022-04-22 15:43:46 +0700

    fix: particles not working :rocket:

--- src/components/ParticleComponent.tsx
+++ src/components/ParticleComponent.tsx
@@ -1,105 +1,114 @@
 import React from 'react';
 import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import type { Engine } from 'tsparticles-engine';
 
-const ParticleComponent = (): JSX.Element => (
-  <div className='bg-black absolute top-0 left-0 w-full'>
-    <Particles
-      id='tsparticles'
-      className='w-full h-screen'
-      options={{
-        background: {
-          color: {
-            value: 'transparent',
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: 'canvas',
-          events: {
-            onClick: {
-              enable: true,
-              mode: 'repulse',
+const ParticleComponent = (): JSX.Element => {
+  const particlesInit = async (main: Engine) => {
+    await loadFull(main);
+  };
+
+  return (
+    <div className='bg-black absolute top-0 left-0 w-full'>
+      <Particles
+        id='tsparticles'
+        className='w-full h-screen'
+        init={particlesInit}
+        options={{
+          background: {
+            color: {
+              value: 'transparent',
             },
-            onHover: {
-              enable: true,
-              mode: 'grab',
-              parallax: {
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: 'canvas',
+            events: {
+              onClick: {
                 enable: true,
-                force: 15,
-                smooth: 10,
+                mode: 'repulse',
+              },
+              onHover: {
+                enable: true,
+                mode: 'grab',
+                parallax: {
+                  enable: true,
+                  force: 15,
+                  smooth: 10,
+                },
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              grab: {
+                links: {
+                  blink: true,
+                  color: '#800000',
+                  consent: true,
+                  opacity: 1,
+                },
+                distance: 169,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
               },
             },
-            resize: true,
           },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
+          particles: {
+            color: {
+              value: '#ff4655',
             },
-            grab: {
-              links: {
-                blink: true,
-                color: '#800000',
-                consent: true,
-                opacity: 1,
-              },
-              distance: 169,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#ff4655',
-          },
-          links: {
-            color: '#ffffff',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: false,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outMode: 'bounce',
-            random: false,
-            speed: 3,
-            straight: false,
-          },
-          number: {
-            density: {
+            links: {
+              color: '#ffffff',
+              distance: 150,
               enable: true,
-              value_area: 800,
+              opacity: 0.5,
+              width: 1,
             },
-            value: 30,
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 30,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'triangle',
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
           },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'triangle',
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-  </div>
-);
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
 
 export default ParticleComponent;
